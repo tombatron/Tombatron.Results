9f63d8b298dbdf4e47bc982b58f846ebdd3324f2
Tommy Hanks <tommy.hanks@gmail.com>
2025-08-30 11:47:07 -0400

    Cleaned up the implementation here a little to avoid the warnings of a switch not being exhaustive.

--- Tombatron.Results/Result.cs
+++ Tombatron.Results/Result.cs
@@ -1,21 +1,18 @@
 ﻿namespace Tombatron.Results;
 
-public abstract class Result<T> where T : notnull
+public class Result<T> where T : notnull
 {
+    internal Result() { }
+    
     public static Result<T> Ok(T value) => 
         new Ok<T>(value);
     
     public static Result<T> Error(string message) => 
         new Error<T>(message);
 
-    public T Unwrap() => this switch
-    {
-        Ok<T> ok => ok.Value,
-
-        Error<T> error => throw new ResultUnwrapException($"[ERROR] [{this.GetType().Name}]: {error.Message}"),
-
-        _ => throw new ResultUnwrapException("Not sure how you did it, but congratulations.")
-    };
+    public T Unwrap() => this is Ok<T> ok 
+        ? ok.Value 
+        : throw new ResultUnwrapException($"[ERROR] [{GetType().Name}]: {(this is Error<T> e ? e.Message : "Unexpected result type.")}");
     
     public T UnwrapOr(T defaultValue) => 
         this is Ok<T> ok ? ok.Value : defaultValue;    

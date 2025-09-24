234c0cc146ad9598f0d43bd4f20e110fab9e7f16
Tom Hanks <tommy.hanks@gmail.com>
2025-09-23 20:14:08 -0400

    Added in child errors, line number and file information.

---
+++ Tombatron.Results/Constants.cs
@@ -0,0 +1,7 @@
+namespace Tombatron.Results;
+
+public static class Constants
+{
+    public static readonly string ErrorHeaderLineSeparator = new string('-', 10);
+    public static readonly string ErrorEntrySeparator = new string('*', 10);
+}
--- Tombatron.Results/Error.cs
+++ Tombatron.Results/Error.cs
@@ -1,14 +1,67 @@
-﻿namespace Tombatron.Results;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using static Tombatron.Results.Constants;
 
-public class Error<T>(string[] messages) : Result<T> where T : notnull
+namespace Tombatron.Results;
+
+public class Error<T> : Result<T>, IErrorResult where T : notnull
 {
-    public Error(string message) : this([message]) { }
+    public IErrorResult? ChildError { get; }
+    
+    private readonly string[] _messages;
+    public string Message => string.Join("\n", Messages);
+    public string[] Messages => ValidateMessages(_messages);
+    
+    public string CallerFilePath { get; }
+    public int CallerLineNumber { get; }
 
-    public string[] Messages { get; } = ValidateMessages(messages);
-
-    private static string[] ValidateMessages(string[] messages)
+    public Error(string message, IErrorResult? childError = null, 
+        [CallerFilePath] string callerFilePath = "",
+        [CallerLineNumber] int callerLineNumber = 0) : this([message], 
+        childError, 
+        // ReSharper disable twice ExplicitCallerInfoArgument
+        callerFilePath,
+        callerLineNumber)
     {
-        if (messages is not {Length: > 0})
+    }
+
+    public Error(string[] messages, IErrorResult? childError = null,
+        [CallerFilePath] string callerFilePath = "",
+        [CallerLineNumber] int callerLineNumber = 0)
+    {
+        _messages = messages;
+        
+        ChildError = childError;
+        
+        CallerFilePath = callerFilePath;
+        CallerLineNumber = callerLineNumber;
+    }
+
+
+
+    public string ToErrorString()
+    {
+        var errorStringBuilder = new StringBuilder();
+        
+        errorStringBuilder.AppendLine($"Error from {this.GetType().Name}");
+        errorStringBuilder.AppendLine(ErrorHeaderLineSeparator);
+        errorStringBuilder.AppendLine("Message(s):");
+        errorStringBuilder.AppendLine(Message);
+        errorStringBuilder.AppendLine($"Path: {CallerFilePath}, Line: #{CallerLineNumber}");
+        
+        if (ChildError is not null)
+        {
+            errorStringBuilder.AppendLine(ErrorEntrySeparator);
+            errorStringBuilder.AppendLine(string.Empty);
+            errorStringBuilder.AppendLine(ChildError.ToErrorString());
+        }
+
+        return errorStringBuilder.ToString();
+    }
+    
+    private string[] ValidateMessages(string[] messages)
+    {
+        if (messages is not { Length: > 0 })
         {
             throw new ArgumentException("At least one error message is required", nameof(messages));
         }
@@ -19,13 +72,15 @@ public class Error<T>(string[] messages) : Result<T> where T : notnull
 
 public class Error(string[] messages) : Result
 {
-    public Error(string message) : this([message]) { }
+    public Error(string message) : this([message])
+    {
+    }
 
     public string[] Messages { get; } = ValidateMessages(messages);
 
     private static string[] ValidateMessages(string[] messages)
     {
-        if (messages is not {Length: > 0})
+        if (messages is not { Length: > 0 })
         {
             throw new ArgumentException("At least one error message is required", nameof(messages));
         }

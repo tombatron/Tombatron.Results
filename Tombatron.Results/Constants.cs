be854d5b3961cee5a9a7b7159fc899c2533895bd
Tom Hanks <tommy.hanks@gmail.com>
2025-09-27 11:42:03 -0400

    A few touch ups.

--- Tombatron.Results/Constants.cs
+++ Tombatron.Results/Constants.cs
@@ -1,6 +1,6 @@
 namespace Tombatron.Results;
 
-public static class Constants
+internal static class Constants
 {
     public static readonly string ErrorHeaderLineSeparator = new string('-', 10);
     public static readonly string ErrorEntrySeparator = new string('*', 10);

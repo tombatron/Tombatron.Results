a3223a6248509a8cec0c37fcecf9f09b4d837e79
Tom Hanks <tommy.hanks@gmail.com>
2025-10-18 16:22:54 -0400

    Added in refactoring

--- Tombatron.Results.Analyzers/Tombatron.Results.Analyzers.Sample/ErrorDetailsExample.cs
+++ Tombatron.Results.Analyzers/Tombatron.Results.Analyzers.Sample/ErrorDetailsExample.cs
@@ -61,4 +61,27 @@ public class OtherConnectionError : IErrorDetails
     public string[] Messages { get; }
     public string CallerFilePath { get; }
     public int CallerLineNumber { get; }
+
+    public OtherConnectionError(IErrorResult? childError, string[] messages, string callerFilePath, int callerLineNumber)
+    {
+        ChildError = childError;
+        Messages = messages;
+        CallerFilePath = callerFilePath;
+        CallerLineNumber = callerLineNumber;
+    }
+
+    public static Result<T> Create<T>(
+            string message,
+            IErrorResult? childError = null,
+            [CallerFilePath] string callerFilePath = "",
+            [CallerLineNumber] int callerLineNumber = 0) where T : notnull =>
+            Create<T>([message], childError, callerFilePath, callerLineNumber);
+
+    public static Result<T> Create<T>(
+        string[] messages,
+        IErrorResult? childError = null,
+        [CallerFilePath] string callerFilePath = "",
+        [CallerLineNumber] int callerLineNumber = 0) where T : notnull =>
+        Result<T>.Error(new OtherConnectionError(childError, messages, callerFilePath, callerLineNumber));
+
 }
